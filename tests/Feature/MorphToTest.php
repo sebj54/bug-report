41bb7a87cf82ab51a18147cf7b07ff48cd9f7234
Sébastien JEAN <contact@sebjean.com>
2024-11-22 10:19:46 +0100

    Add reproduction scenario

---
+++ tests/Feature/MorphToTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Image;
+use App\Models\Post;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class MorphToTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_morph_to_has_correct_where(): void
+    {
+        $post = Post::factory()->create();
+
+        $image = Image::factory()
+            ->for($post, 'imageable')
+            ->create();
+
+        $relation = $image->imageable();
+        $wheres = $relation->getQuery()->getQuery()->wheres;
+
+        $this->assertContains([
+            'type' => 'Basic',
+            'column' => 'posts.id',
+            'operator' => '=',
+            'value' => $post->id,
+            'boolean' => 'and',
+        ], $wheres);
+    }
+
+    public function test_empty_morph_to_relation_has_correct_where(): void
+    {
+        $relation = (new Image())->imageable();
+        $wheres = $relation->getQuery()->getQuery()->wheres;
+
+        $this->assertNotContains([
+            'type' => 'Null',
+            'column' => '',
+            'boolean' => 'and',
+        ], $wheres);
+    }
+}
